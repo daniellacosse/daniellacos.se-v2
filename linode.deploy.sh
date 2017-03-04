95f1e9330072f8fdbca260e3340a6f7bc53d45e0
Daniel LaCosse <d@niellacos.se>
2017-03-04 10:48:34 -0500

    adds yarn

--- linode.deploy.sh
+++ linode.deploy.sh
@@ -22,13 +22,13 @@ rm ${build}
 current=`date +%s`
 echo -ne " ...installing dependencies ($((current-start))s elapsed) \r"
 
-npm --production i > ./_build.log 2>&1 || ( EC=$?; cat ./_build.log; exit $EC )
+yarn --production --silent > /dev/null 2>&1
 
 current=`date +%s`
 echo -ne " ...restarting server ($((current-start))s elapsed)      \r"
 
 cp /home/daniel/app/.secrets /etc/environment
-sudo -E pm2 restart server.js > ./_build.log 2>&1 || ( EC=$?; cat ./_build.log; exit $EC )
+sudo -E pm2 restart server.js > /dev/null 2>&1
 
 current=`date +%s`
 echo -ne " ...caching index ($((current-start))s elapsed)          \r"
--- linode.setup.sh
+++ linode.setup.sh
@@ -53,4 +53,9 @@ cd ~
 mkdir app
 mkdir builds
 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update && sudo apt-get install yarn
+
 # then run gulp deploy locally
